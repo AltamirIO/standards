86e577d5b9f51f8f918db2748b5952f320505d12
Jacob Dick <jacobkdick@gmail.com>
2018-12-29 15:20:18 -0700

    add rules to tslint

--- packages/standards-tslint/index.js
+++ packages/standards-tslint/index.js
@@ -2,4 +2,9 @@ module.exports = {
   extends: [
     'tslint:recommended',
   ],
+  'rules': {
+    'object-literal-sort-keys': false,
+    'quotemark': [true, 'single'],
+    'semicolon': [true, 'never'],
+},
 }
